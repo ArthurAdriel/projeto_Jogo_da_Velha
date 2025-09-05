c3f931e3fd6e9c48bad06d427ee35200d9962ee7
Arthur Adriel <arthurtorquato25@gmail.com>
2025-09-05 18:34:34 -0300

    Projeto
    Fazendo o X do jogo da velha, e o início do do O

--- index.py
+++ index.py
@@ -3,6 +3,8 @@ import pygame
 pygame.init()
 
 largura, altura = 600, 600
+cont = 1
+
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Jogo da Velha')
 
@@ -16,10 +18,44 @@ pygame.draw.line(tela, preto, (400, 0), (400, 600), 5)
 pygame.draw.line(tela, preto, (0, 200), (600, 200), 5)
 pygame.draw.line(tela, preto, (0, 400), (600, 400), 5)
 
+def desenhar_x(linha, coluna):
+    x = coluna * 200
+    y = linha * 200
+    margem = 30
+    pygame.draw.line(tela, preto, (x+margem, y+margem), (x+200-margem, y+200-margem), 10)
+    pygame.draw.line(tela, preto, (x+200-margem, y+margem), (x+margem, y+200-margem), 10)
+
+def desenhar_o(linha, coluna):
+    x = coluna * 200
+    y = linha * 200
+    margem = 30
+    pygame.draw.circle(tela, preto, (x+margem, y+margem), (x+200-margem, y+200-margem), 10)
+    pygame.draw.circle(tela, preto, (x+200-margem, y+margem), (x+margem, y+200-margem), 10)
+
 rodando = True
 while rodando:
     for evento in pygame.event.get():
-        if evento.type == pygame.quit():
+        if evento.type == pygame.QUIT:
             rodando = False
+
+        if cont % 2 == 0:
+
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                x, y = evento.pos
+                coluna = x // 200
+                linha = y // 200
+                desenhar_o(linha, coluna)
+
+        else:
+            
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                x, y = evento.pos
+                coluna = x // 200
+                linha = y // 200
+                desenhar_x(linha, coluna)
+
+            cont += 1
+
+    pygame.display.update()
 
 pygame.quit()
