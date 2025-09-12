f806604a91fac58cac9130edd3e978fcda086b27
Arthur Adriel <arthurtorquato25@gmail.com>
2025-09-12 12:00:02 -0300

    Projeto
    Fiz uma tela de início

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ pygame.init()
 
 largura, altura = 600, 600
 cont = 0
+rodar_jogo = False
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Jogo da Velha')
@@ -13,10 +14,22 @@ preto = (0, 0, 0)
 
 tela.fill((branco))
 
-pygame.draw.line(tela, preto, (200, 0), (200, 600), 5)
-pygame.draw.line(tela, preto, (400, 0), (400, 600), 5)
-pygame.draw.line(tela, preto, (0, 200), (600, 200), 5)
-pygame.draw.line(tela, preto, (0, 400), (600, 400), 5)
+pygame.draw.rect(tela, preto, (200, 250, 200, 100), 0)
+
+fonte_botao = pygame.font.SysFont('Open Sans', 30)
+msg_botao = fonte_botao.render('Iniciar o Jogo', True, branco)
+tela.blit(msg_botao, (232, 292))
+
+fonte_titulo = pygame.font.SysFont('Open Sans', 70)
+titulo = fonte_titulo.render('Jogo da Velha', True, preto)
+tela.blit(titulo, (130, 50))
+
+def desenhar_grade():
+    tela.fill((branco))
+    pygame.draw.line(tela, preto, (200, 0), (200, 600), 5)
+    pygame.draw.line(tela, preto, (400, 0), (400, 600), 5)
+    pygame.draw.line(tela, preto, (0, 200), (600, 200), 5)
+    pygame.draw.line(tela, preto, (0, 400), (600, 400), 5)
 
 def desenhar_x(linha, coluna):
     x = coluna * 200
@@ -40,16 +53,23 @@ while rodando:
 
         if evento.type == pygame.MOUSEBUTTONDOWN:
             x, y = evento.pos
-            coluna = x // 200
-            linha = y // 200
 
-            if cont % 2 == 0:
-                desenhar_o(linha, coluna)
+            if rodar_jogo == False:
+                if x > 200 and x < 400 and y > 250 and y < 350:
+                    rodar_jogo = True
+                    desenhar_grade()
             else:
-                desenhar_x(linha, coluna)
-                
-            cont += 1
+                coluna = x // 200
+                linha = y // 200
+
+                if cont % 2 == 0:
+                    desenhar_o(linha, coluna)
+                else:
+                    desenhar_x(linha, coluna)
+                            
+                cont += 1
 
     pygame.display.update()
 
 pygame.quit()
+print(cont)
