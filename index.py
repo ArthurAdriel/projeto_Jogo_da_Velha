c45b5f7d3c2e74297cd9ff004f754075e3e042c4
Arthur Adriel <arthurtorquato25@gmail.com>
2025-09-16 12:05:16 -0300

    Projeto
    Fiz um sistema para descobrir quem ganhou, e fiz um sistema de reinicio.

--- index.py
+++ index.py
@@ -1,29 +1,33 @@
 import pygame
+import time
 
 pygame.init()
 
 largura, altura = 600, 600
-cont = 0
-rodar_jogo = False
-tabuleiro = [[None for _ in range(3)] for _ in range(3)]
-
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Jogo da Velha')
+
+cont = 0
+tabuleiro = [[None for _ in range(3)] for _ in range(3)]
+vencedor = None
+tempo_fim = None
+estado = 'menu'
 
 branco = (255, 255, 255)
 preto = (0, 0, 0)
 
-tela.fill((branco))
+def desenhar_menu():
+    tela.fill((branco))
 
-pygame.draw.rect(tela, preto, (200, 250, 200, 100), 0)
+    pygame.draw.rect(tela, preto, (200, 250, 200, 100), 0)
 
-fonte_botao = pygame.font.SysFont('Open Sans', 30)
-msg_botao = fonte_botao.render('Iniciar o Jogo', True, branco)
-tela.blit(msg_botao, (232, 292))
+    fonte_botao = pygame.font.SysFont('Open Sans', 30)
+    msg_botao = fonte_botao.render('Iniciar o Jogo', True, branco)
+    tela.blit(msg_botao, (232, 292))
 
-fonte_titulo = pygame.font.SysFont('Open Sans', 70)
-titulo = fonte_titulo.render('Jogo da Velha', True, preto)
-tela.blit(titulo, (130, 50))
+    fonte_titulo = pygame.font.SysFont('Open Sans', 70)
+    titulo = fonte_titulo.render('Jogo da Velha', True, preto)
+    tela.blit(titulo, (130, 50))
 
 def desenhar_grade():
     tela.fill((branco))
@@ -46,6 +50,24 @@ def desenhar_o(linha, coluna):
     pygame.draw.circle(tela, preto, (x, y), raio, 10)
     pygame.draw.circle(tela, preto, (x, y), raio, 10)
 
+def verificar_vencedor(tabuleiro):
+    for linha in tabuleiro:
+        if linha[0] == linha[1] == linha[2] and linha[0] is not None:
+            return linha[0]
+    
+    for c in range(3):
+        if tabuleiro[0][c] == tabuleiro[1][c] == tabuleiro[2][c] and tabuleiro[0] is not None:
+            return tabuleiro[0][c]
+        
+    if tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2] and tabuleiro[0][0] is not None:
+        return tabuleiro[0][0]
+    
+    if tabuleiro[0][2] == tabuleiro[1][1] == tabuleiro[2][0] and tabuleiro[0][2] is not None:
+        return tabuleiro[0][2]
+
+
+desenhar_menu()
+
 rodando = True
 while rodando:
     for evento in pygame.event.get():
@@ -57,13 +79,15 @@ while rodando:
             coluna = x // 200
             linha = y // 200
             
-            if rodar_jogo == False:
+            if estado == 'menu':
                 if x > 200 and x < 400 and y > 250 and y < 350:
-                    rodar_jogo = True
+                    estado = 'jogando'
+                    cont = 0
+                    vencedor = None
+                    tabuleiro = [[None for _ in range(3)] for _ in range(3)]
                     desenhar_grade()
-            else:
+            elif estado == 'jogando':
                 if tabuleiro[linha][coluna] is None:
-
                     if cont % 2 == 0:
                         desenhar_o(linha, coluna)
                         tabuleiro[linha][coluna] = 'O'
@@ -72,7 +96,16 @@ while rodando:
                         tabuleiro[linha][coluna] = 'X'
                                 
                     cont += 1
+            vencedor = verificar_vencedor(tabuleiro)
+            if vencedor is not None or cont == cont == 9:
+                estado = 'fim'
+                tempo_fim = time.time()
 
+    if estado == 'fim':
+        if time.time() - tempo_fim >= 2:
+            desenhar_menu()
+            estado = 'menu'
     pygame.display.update()
 
 pygame.quit()
+print(vencedor)
